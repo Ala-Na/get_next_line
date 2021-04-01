30617e8db2dbf64da431f0c1612e0ff5beaae0f6
anadege <ilmefautuneadresse@gmail.com>
2021-04-01 20:56:32 +0200

    First commit

---
+++ get_next_line.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elanna <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/27 17:01:40 by elanna            #+#    #+#             */
+/*   Updated: 2021/04/01 20:53:04 by elanna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+t_content	*ft_lstchr_fd(t_content **lst, int fd)
+{
+	t_content	*list;
+
+	list = *lst;
+	while (list)
+	{
+		if (fd == list->fd)
+			return (list);
+		list = list->next;
+	}
+	return (NULL);
+}
+
+int			get_content_line(int fd, char **line, ssize_t rd, t_content **cont)
+{
+	t_content	*fd_content;
+	char		*ptr;
+	char		*tmp;
+
+	if (*cont && !(fd_content = ft_lstchr_fd(cont, fd)))
+		return (-1);
+	if (rd == 0 && (!*cont || !(fd_content->buffer)))
+	{
+		*line = ft_strdup("");
+		return (0);
+	}
+	ptr = ft_strchr(fd_content->buffer, '\n');
+	if (ptr != NULL)
+	{
+		*ptr = '\0';
+		*line = ft_strdup((const char*)fd_content->buffer);
+		tmp = ft_strdup(++ptr);
+		free(fd_content->buffer);
+		fd_content->buffer = tmp;
+		return (1);
+	}
+	*line = fd_content->buffer;
+	fd_content->buffer = NULL;
+	return (0);
+}
+
+void		fill_content_struct(t_content **content, char *buffer, int fd)
+{
+	char		*tmp_buffer;
+	t_content	*fd_content;
+
+	if (!*content)
+		*content = ft_lstnew(buffer, fd);
+	else
+	{
+		fd_content = ft_lstchr_fd(content, fd);
+		if (fd_content != NULL)
+		{
+			tmp_buffer = ft_strjoin(fd_content->buffer, buffer);
+			free(fd_content->buffer);
+			fd_content->buffer = tmp_buffer;
+		}
+		else
+			ft_lstadd_back(content, ft_lstnew(buffer, fd));
+	}
+}
+
+
+int			get_next_line(int fd, char **line)
+{
+	static t_content	*content;
+	char				buffer[BUFFER_SIZE + 1];
+	t_content			*fd_content;
+	ssize_t				size_read;
+	char				*new_line;
+
+	if (fd < 0 || line == NULL || BUFFER_SIZE <= 0)
+		return (-1);
+	while ((size_read = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		buffer[size_read] = '\0';
+		fill_content_struct(&content, buffer, fd);
+		fd_content = ft_lstchr_fd(&content, fd);
+		if (!fd_content)
+			return (-1);
+		new_line = ft_strchr(fd_content->buffer, '\n');
+		if (new_line != NULL)
+			break ;
+	}
+	if (size_read < 0)
+		return (-1);
+	return (get_content_line(fd, line, size_read, &content));
+}
---
+++ get_next_line.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elanna <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/27 17:01:23 by elanna            #+#    #+#             */
+/*   Updated: 2021/04/01 15:30:04 by elanna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct	s_content
+{
+	char				*buffer;
+	int					fd;
+	struct s_content	*next;
+}				t_content;
+
+char			*ft_strdup(const char *s);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strchr(const char *s, int c);
+t_content		*ft_lstnew(char *buffer, int fd);
+void			ft_lstadd_back(t_content **alst, t_content *new);
+t_content		*ft_lstchr_fd(t_content **lst, int fd);
+int				get_content_line(int fd, char **line, ssize_t rd,
+					t_content **cont);
+void			fill_content_struct(t_content **content, char *buffer, int fd);
+int				get_next_line(int fd, char **line);
+
+#endif
