4fbffdeb88fff903383b28049c40efdb384da457
anadege <ilmefautuneadresse@gmail.com>
2021-04-01 21:54:13 +0200

    Made copy with _bonus

---
+++ bonus/get_next_line_utils_bonus.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elanna <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/27 17:01:55 by elanna            #+#    #+#             */
+/*   Updated: 2021/04/01 20:20:01 by elanna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char		*ft_strdup(const char *s)
+{
+	int		size;
+	char	*dest;
+	int		i;
+
+	i = 0;
+	size = 0;
+	while (s[size])
+		size++;
+	if (!(dest = malloc(sizeof(*dest) * (size + 1))))
+		return (0);
+	while (s[i] != 0)
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	size;
+	char	*str;
+
+	i = -1;
+	size = 0;
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	if (!s2 && s1)
+		return (ft_strdup(s1));
+	if (!s1 && !s2)
+		return (NULL);
+	while (s1[size])
+		size++;
+	while (s2[++i])
+		size++;
+	if (!(str = malloc(sizeof(*str) * (size + 1))))
+		return (0);
+	i = 0;
+	while (*s1)
+		str[i++] = *s1++;
+	while (*s2)
+		str[i++] = *s2++;
+	str[size] = 0;
+	return (str);
+}
+
+char		*ft_strchr(const char *s, int c)
+{
+	while (s && *s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == 0 && *s == 0)
+		return ((char *)s);
+	return (NULL);
+}
+
+t_content	*ft_lstnew(char *buffer, int fd)
+{
+	t_content	*new;
+
+	if (!(new = malloc(sizeof(*new))))
+		return (NULL);
+	new->buffer = ft_strdup(buffer);
+	new->fd = fd;
+	new->next = NULL;
+	return (new);
+}
+
+void		ft_lstadd_back(t_content **alst, t_content *new)
+{
+	t_content *last;
+
+	if (!new)
+		return ;
+	else if (!*alst)
+	{
+		*alst = new;
+		return ;
+	}
+	last = *alst;
+	while (last->next)
+		last = last->next;
+	last->next = new;
+}
